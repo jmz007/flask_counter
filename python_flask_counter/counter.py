158dce9b1ba4f30fffffc6ff693cb8aa43061e58
James Chaney <42975010+jmz007@users.noreply.github.com>
2018-11-04 14:19:27 -0600

    Add files via upload

---
+++ python_flask_counter/counter.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)  
+app.secret_key = 'mycount'
+
+@app.route('/')
+def count():
+	if "count" in session:
+		session["count"] += 1
+	else:
+		session["count"] = 1
+	return render_template('index.html', count=session["count"])
+
+@app.route('/reset')
+def reset():
+	session.clear()
+	return redirect ('/')
+
+@app.route('/add')
+def add():
+	session["count"] += 1
+	return redirect ('/')
+
+if __name__=="__main__":   
+    app.run(debug=True)    
